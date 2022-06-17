727300af8ae1d90248b6fb9f62bff7926e25d9e1
malvina17girl <malvina17girl@yandex.ru>
2022-06-17 18:56:09 +0600

    решена задача 6 из пр3

---
+++ Пр3 задача 6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Пр3_задача_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String data;
+            Console.WriteLine("вводите значение X");
+            data = Console.ReadLine();
+            double X;
+            X = double.Parse(data);
+            if (X <= 0)
+            {
+
+                Console.WriteLine("Подкоренное выражение должно быть неотрицательным");
+                return; //завершить работу прогр
+            }
+            double result;
+            result = Math.Sqrt(X);
+
+            Console.WriteLine(" Результат= {0:F0}", result);
+        }
+    }
+}
