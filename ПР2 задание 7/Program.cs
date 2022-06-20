6e7949cad2d8e3c2ada7db95ca2abca78c3a6cfe
malvina17girl <malvina17girl@yandex.ru>
2022-06-20 18:10:26 +0600

    второй

---
+++ ПР2 задание 7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ПР2_задание_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String data;
+            Console.WriteLine("вводите значение скорость");
+            data = Console.ReadLine();
+            double knots;
+            knots = double.Parse(data);
+            double result;
+            result = knots * 2 * 0.9;
+
+            Console.Write("{0:F2} узлов это приблезительно {1:F2}", knots, result);
+            Console.WriteLine(" км/ч");
+
+        }
+    }
+}
